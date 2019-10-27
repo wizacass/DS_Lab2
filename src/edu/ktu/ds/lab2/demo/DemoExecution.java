7294e494e4f09853b9bdb7e7224d50676fb89d58
Wizacass <visak.pet0@gmail.com>
2019-10-27 18:51:12 +0200

    Initial commit

---
+++ src/edu/ktu/ds/lab2/demo/DemoExecution.java
@@ -0,0 +1,27 @@
+package edu.ktu.ds.lab2.demo;
+
+import edu.ktu.ds.lab2.gui.MainWindow;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Locale;
+
+/*
+ * Darbo atlikimo tvarka - čia yra pradinė klasė.
+ */
+public class DemoExecution extends Application
+{
+
+    public static void main(String[] args)
+    {
+        DemoExecution.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Locale.setDefault(Locale.US); // Suvienodiname skaičių formatus 
+        ManualTest.executeTest();
+        MainWindow.createAndShowGui(primaryStage);
+    }
+}
